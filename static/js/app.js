c75053f2046ee098631e44cf221e4cae2642365c
Goffin Simon <simon.goffin@outlook.com>
2014-07-13 17:23:44 +0200

    Usage of Deferreds.

--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,44 @@
 
     function main() {
         $("button").click(function() {
-            _.each(_.range(1, 11), function(i) {
-                $(".main_content").append("<div>" + i + "</div>");
+            plusmultplus(1, 2, 3, 4).then(function(result) {
+                console.log("(1+2)*(3+4)=", result.multiplication);
             });
         });
     };
     app.main = main;
+
+    function plusmultplus(a, b, c, d) {
+        var def1 = $.ajax("/service_plus", {
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                "a": a,
+                "b": b,
+            }),
+            contentType: "application/json",
+        });
+        var def2 = $.ajax("/service_plus", {
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                "a": c,
+                "b": d,
+            }),
+            contentType: "application/json",
+        });
+
+        return $.when(def1, def2).then(function(result1, result2) {
+            return $.ajax("/service_mult", {
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    "a": result1[0].addition,
+                    "b": result2[0].addition,
+                }),
+                contentType: "application/json",
+            });
+        });
+    };
 
 })();
